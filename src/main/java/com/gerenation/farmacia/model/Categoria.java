26ebe21910db244ef10cbe4213dd2b0af80887c3
Taina Poppi <tainapoppi@hotmail.com>
2024-03-26 10:16:20 -0300

    CRUD - Farmacia Taks1

---
+++ src/main/java/com/gerenation/farmacia/model/Categoria.java
@@ -0,0 +1,24 @@
+package com.gerenation.farmacia.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+}
